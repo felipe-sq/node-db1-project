3bc44665a8644db482275872a8f984271f4818fd
Felipe Slaughter-Quintero <fslauq@gmail.com>
2021-08-18 22:01:25 -0400

    code for middleware completed.

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,36 @@
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  if (!req.body.name || !req.body.budget) {
+    return res.status(400).json({
+      message: 'name and budget are required'
+    });
+  } else if (typeof req.body.name !== 'string') { 
+    return res.status(400).json({message: 'name of account must be a string'});
+  } else if (req.body.name.trim().length < 3 || req.body.name.trim().length > 100) {
+    return res.status(400).json({message: 'name of account must be between 3 and 100'});
+  } else if (typeof req.body.budget !== 'number') {
+    return res.status(400).json({message: 'budget of account must be a number'});
+  } else if (req.body.budget < 0 || req.body.budget > 1000000) {
+    return res.status(400).json({message: 'budget of account is too large or too small'});
+  } else {
+    next();
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
+  if (req.body.name.trim() === req.params.name.trim()) {
+    return res.status(400).json({message: 'that name is taken'});
+  } else {
+    next();
+  }
 }
 
 exports.checkAccountId = (req, res, next) => {
   // DO YOUR MAGIC
+  if (!req.params.id) {
+    return res.status(404).json({message: 'account not found'});
+  } else {
+    next();
+  }
 }
