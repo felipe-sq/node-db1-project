35e4189a24c2d29c429500eafb88c50fff92e79c
Felipe Slaughter-Quintero <fslauq@gmail.com>
2021-08-18 22:14:00 -0400

    completed code for API endpoints with middleware.

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,64 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const Account = require('./accounts-model.js');
 
-router.get('/', (req, res, next) => {
+const {checkAccountId, checkAccountNameUnique, checkAccountPayload} = require('./accounts-middleware.js');
+
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const accounts = await Account.getAll()
+    res.status(200).json(accounts)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const account = await Account.getById(req.params.id)
+    res.status(200).json(account)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAccountPayload, checkAccountNameUnique, async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const account = await Account.create(req.body)
+    res.status(201).json(account)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', checkAccountId, checkAccountPayload, async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const account = await Account.updateById(req.params.id, req.body)
+    res.status(200).json(account)
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const account = await Account.deleteById(req.params.id)
+    res.status(204).json(account)
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  })
 })
 
 module.exports = router;
