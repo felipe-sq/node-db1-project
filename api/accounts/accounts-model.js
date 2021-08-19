331c8979aee4bb339b1be8820e5cd972ee2d47fc
Felipe Slaughter-Quintero <fslauq@gmail.com>
2021-08-18 21:34:59 -0400

    model functions completed for accounts-model.js

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,32 @@
-const getAll = () => {
+const db = "../../data/db-config.js";
+
+async function getAll() {
   // DO YOUR MAGIC
+  return db('accounts');
 }
 
-const getById = id => {
+async function getById(id)  {
   // DO YOUR MAGIC
+  return db('accounts').where("id", id).first();
 }
 
-const create = account => {
+async function create({name, budget}) {
   // DO YOUR MAGIC
+  const [id] = await db('accounts').insert({name, budget});
+  return getById(id);
 }
 
-const updateById = (id, account) => {
+async function updateById(id, {name, budget}) {
   // DO YOUR MAGIC
+  await db('accounts').where("id", id).update({name, budget});
+  return getById(id);
 }
 
-const deleteById = id => {
+async function deleteById(id) {
   // DO YOUR MAGIC
+  const deletedAccount = await getById(id);
+  await db('accounts').where("id", id).delete();
+  return deletedAccount;
 }
 
 module.exports = {
